3b53b00a35b81b79ca57ca54ec0c408a3c4ec98c
Nicholas Wease <skynet60@outlook.com>
2019-06-18 11:06:56 +0200

    Deleted a bunch of crap.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,20 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+// import './bootstrap.min.css'
+// import './layout.css'
+import './sass/layout.scss'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+const Layout = ({ children }) => {
+  return (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  )
+}
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export default Layout
