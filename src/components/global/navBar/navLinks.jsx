5aa3f20a52d05cbf1e9742415c01d8dc1dec4ffd
Nicholas Wease <skynet60@outlook.com>
2019-06-21 13:50:58 +0200

    More styling of navbar and navbar links.

--- src/components/global/navBar/navLinks.jsx
+++ src/components/global/navBar/navLinks.jsx
@@ -34,6 +34,7 @@ class NavLinks extends Component {
   }
 
   render() {
+    // console.log(this.props.navBarOpen);
     return (
       <LinkWrapper open={this.props.navBarOpen}>
         {
@@ -55,6 +56,38 @@ class NavLinks extends Component {
 const LinkWrapper = styled.ul `
   li {
     list-style-type: none;
+  }
+  
+  .nav-link {
+    display: block;
+    text-decoration: none;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    color: ${styles.colors.mainGrey};
+    font-weight: 700;
+    text-transform: capitalize;
+    cursor: pointer;
+    ${styles.transDefault};
+    
+    &:hover {
+      background: ${styles.colors.mainGrey};
+      color: ${styles.colors.mainYellow};
+      padding: 0.5rem 1rem 0.5rem 1.3rem;
+    }
+  }
+  
+  height: ${props => (props.open ? '152px' : '0px')};
+  overflow: hidden;
+  ${styles.transObject({time: '0.8s'})};
+  
+  @media (min-width: 768px) {
+    height: auto;
+    display: flex;
+    margin: 0 auto;
+    
+    .nav-link:hover {
+      background: ${styles.colors.mainWhite};
+      padding: 0.5rem 1rem 0.5rem 1rem;
+    }
   }
 `
 
